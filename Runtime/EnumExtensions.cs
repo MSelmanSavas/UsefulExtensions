bd1d0862151b07927003027defc10d9add0ec696
MehmetSelmanSavas <mehmetselmansavas@gmail.com>
2026-05-22 18:10:02 +0300

    Added enum extensions for cycling through enums

--- Runtime/EnumExtensions.cs
+++ Runtime/EnumExtensions.cs
@@ -14,6 +14,18 @@ namespace UsefulExtensions.Enum
         {
             return (int)(object)e;
         }
+
+        public static T Next<T>(this T src) where T : struct, System.Enum
+        {
+            // Get all values of the enum as an array
+            T[] arr = (T[])System.Enum.GetValues(src.GetType());
+
+            // Find the index of the current value
+            int j = System.Array.IndexOf(arr, src) + 1;
+
+            // If we reached the end, wrap around to 0
+            return (arr.Length == j) ? arr[0] : arr[j];
+        }
     }
 }
 
